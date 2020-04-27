72054bdc44c3a88a95d25351feac3bc39a2efe2d
zouhecan <hc.zou@ctrip.com>
2020-04-27 15:40:37 +0800

    rebase for network permission

--- app/src/main/java/com/example/googlemapapp/MapsActivity.kt
+++ app/src/main/java/com/example/googlemapapp/MapsActivity.kt
@@ -26,9 +26,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         Handler().postDelayed(Runnable {
             val urls = ArrayList(
                 mutableListOf(
-                    "https://help.github.com/cn/github/using-git/resolving-merge-conflicts-after-a-git-rebase",
-                    "https://juejin.im/entry/5ae9706d51882567327809d0",
-                    "https://juejin.im/entry/5ae9706d51882567327809d0",
+//                    "https://help.github.com/cn/github/using-git/resolving-merge-conflicts-after-a-git-rebase",
+//                    "https://juejin.im/entry/5ae9706d51882567327809d0",
+//                    "https://juejin.im/entry/5ae9706d51882567327809d0",
                     "https://zhuanlan.zhihu.com/p/32536915"
                 )
             )
